9d18c1d2f3a260ad081fc6a08d6c0e162e475e43
Agrim Pradhananga <agrimp91@gmail.com>
2024-11-20 14:06:20 +0545

    payment failure message

---
+++ app/(root)/payment-failure/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PaymentFailure = () => {
+  return (
+    <div className="section_container">
+      <p className="text-3xl font-bold text-center">Ticket Purchase Failed</p>
+    </div>
+  )
+}
+
+export default PaymentFailure
